2aaff8fb4b143ce53cc92a07ab8bfc72fd0cf0ec
riht7405 <fly200fly@gmail.com.com>
2025-11-25 02:11:45 +0500

    Низамов Айдар Модуль 4, Задание 1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,12 +1,24 @@
 package com.example.task01;
 
-/**
- * Класс точки на плоскости
- */
 public class Point {
     int x;
     int y;
-
+    Point(){}
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    void flip() {
+        int temp = x;
+        x = (-1) * y;
+        y = (-1) * temp;
+    }
+    double distance(Point point) {
+        return Math.sqrt((y - point.y) * (y - point.y) + (x - point.x) * (x - point.x));
+    }
+    public String toString() {
+        return "(" + Integer.toString(x) + ", " + Integer.toString(y) + ")";
+    }
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
